50b5e63104c285b84b1b3b1169b012724965ded1
zhangqingjie <zhangqingjie@doodlemobile.com>
2018-05-31 16:08:29 +0800

    css

--- blog/views.py
+++ blog/views.py
@@ -7,17 +7,21 @@ def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
     context['blogs_count'] = Blog.objects.all().count()
-    return render_to_response('blog_list.html', context)
+    context['blog_types'] = BlogType.objects.all()
+    return render_to_response('blog/blog_list.html', context)
     
 
 def blog_detail(request, blog_pk): #pk指的是组件
     context = {}
     context['blog'] = get_object_or_404(Blog, id=blog_pk)
-    return render_to_response('blog_detail.html', context)
+    print("blog_pk: ", blog_pk)
+    print("request: ", request)
+    return render_to_response('blog/blog_detail.html', context)
 
 def blogs_with_type(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
-    return render_to_response('blogs_with_type.html', context)
+    context['blog_types'] = BlogType.objects.all()
+    return render_to_response('blog/blogs_with_type.html', context)
